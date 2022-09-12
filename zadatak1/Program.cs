570b297e84f7c571e225c34b5fedfd77815bd069
sebastian krnjus <sebastiankrnjus25@gmail.com>
2022-09-12 11:16:39 +0200

    Add project files.

---
+++ zadatak1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 3;
+            float ostatak = a/b;
+
+            Console.WriteLine("a podijeljeno s b je " +  Math.Abs(a/b) + " a ostatak je " + ".5"+ Math.Truncate(ostatak));
+
+
+            Console.ReadKey();
+        }
+    }
+}
